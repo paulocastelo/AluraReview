8d115f19116b8530709d6b69e8281f70975a2963
Paulo Anderson Oliveira Castelo <paulo.castelo@actdigital.com>
2021-12-15 18:23:01 -0300

    Commit

---
+++ CSharpPT9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,53 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ByteBankImportacaoExportacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        static void LidandoComOFileStreamDiretamente()
+        {
+            var enderecoArquivo = "contas.txt";
+
+            var fluxoDoArquivo = new FileStream(enderecoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+
+            var numeroDeBytesLidos = -1;
+
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+
+            }
+
+            fluxoDoArquivo.Close();
+
+            Console.ReadLine();
+        }
+        static void EscreverBuffer(byte[] buffer)
+        {
+            var utf8 = new UTF8Encoding();
+
+            var texto = utf8.GetString(buffer);
+            Console.WriteLine(texto);
+
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
+        }
+    }
+}
